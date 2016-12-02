e45b4f88e5d811dd77bef02ae4539f871f81a1ab
Paul McCrory <paulmccrory@hotmail.co.uk>
2016-12-02 12:32:38 +0000

    gitignore

--- routes/genres.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// Home Page
-router.get('/', function(req, res, next) {
-  	res.render('genres/index');
-});
-
-module.exports = router;
