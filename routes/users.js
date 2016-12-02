1b62ee4fb50fc586fd9c6dfc8b53a3611fe2f60f
Paul McCrory <paulmccrory@hotmail.co.uk>
2016-12-02 12:35:21 +0000

    Another update

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+// Home Page
+router.get('/register', function(req, res, next) {
+  	res.render('/users/register');
+});
+
+router.get('/login', function(req, res, next) {
+  	res.render('/users/login');
+});
+
+module.exports = router;
