0388049bb0e864cfe531f7a68526c631acc266d2
Paul McCrory <paulmccrory@hotmail.co.uk>
2016-12-03 14:22:16 +0000

    Add game POST route

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Firebase = require('firebase');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // GET - Home Page
 router.get('/', function (req, res, next) {
@@ -11,10 +13,10 @@ router.get('/', function (req, res, next) {
 router.get('/add', function (req, res, next) {
 
 	// Gets a reference to the database and table.
-	var gameReference = Firebase.database().ref('genres');
+	var genreReference = Firebase.database().ref('genres');
 
 	// Iterates through a snapshot of the chosen database table then grabs the key/val for the view.
-	gameReference.once('value').then(function(snapshot) {
+	genreReference.once('value').then(function(snapshot) {
 		var genres = [];
 		var jsonData = snapshot.val();
 
@@ -29,15 +31,29 @@ router.get('/add', function (req, res, next) {
 });
 
 // POST - Add Game
-router.post('/add', function (req, res, next) {
-	var genre = {
-		name: req.body.name
+router.post('/add', upload.single('cover'), function (req, res, next) {
+	var cover = 'noimage.jpg'
+	
+	if(req.file){
+		cover = req.file.filename;
+	}
+
+	var gameObj = {
+		game_name : req.body.game_name,
+		game_developer : req.body.game_developer,
+		game_publisher : req.body.game_publisher,
+		year_released : req.body.year_released,
+		genre : req.body.genre,
+		review : req.body.review,
+		rating : req.body.rating,
+		cover : cover
 	}
 
 	var gameReference = Firebase.database().ref('games');
-	gameReference.push().set(genre);
+	
+	gameReference.set(gameObj);
 
-	req.flash('success_msg', 'Game has been saved.')
+	req.flash("success_msg", "Game entry has been saved.");
 	res.redirect('/games');
 });
 
