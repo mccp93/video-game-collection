b8628708007f70cb63442b8eeb3aab5760ef99bf
Paul McCrory <paulmccrory@hotmail.co.uk>
2016-12-04 20:06:26 +0000

    Added edit route as well as relevant front-end for it.

--- routes/games.js
+++ routes/games.js
@@ -84,5 +84,71 @@ router.post('/add', upload.single('cover'), function (req, res, next) {
 	res.redirect('/games');
 });
 
+// GET - Game Details
+router.get('/details/:id', function(req, res){
+	var id = req.params.id;
+
+	var gameReference = Firebase.database().ref('games/' + id);
+
+	// Iterates through a snapshot of the chosen database table then grabs the key/val for the view.
+	gameReference.once('value').then(function (snapshot) {
+		var game = snapshot.val();
+
+		res.render('games/details', { game: game, id: id});
+	});
+});
+
+
+// GET - Edit game
+router.get('/edit/:id', function(req, res, next){
+	var id = req.params.id;
+	var gameReference = Firebase.database().ref('games/' + id);
+	var genreReference = Firebase.database().ref('genres');
+
+	// Iterates through a snapshot of the chosen database table then grabs the key/val for the view.
+	genreReference.once('value').then(function(snapshot) {
+		var genres = [];
+		var jsonData = snapshot.val();
+
+		for(var key in jsonData){
+			if(jsonData.hasOwnProperty(key)){
+				genres.push({id: key, name: jsonData[key].name});
+			}
+		}
+
+		gameReference.once('value').then(function (snapshot) {
+			var game = snapshot.val();
+			res.render('games/edit', { game: game, genres: genres, id: id});
+		});
+	});
+});
+
+
+// POST - Edit game
+router.post('/edit/:id', upload.single('cover'), function(req, res, next){
+	var id = req.params.id;
+	var gameReference = Firebase.database().ref('games/' + id);
+
+	if(req.file){
+		var cover = req.file.filename;
+		gameReference.update({
+			cover : cover
+		});
+	}
+
+	gameReference.update({
+		game_name : req.body.game_name,
+		game_developer : req.body.game_developer,
+		game_publisher : req.body.game_publisher,
+		year_released : req.body.year_released,
+		genre : req.body.genre,
+		review : req.body.review,
+		rating : req.body.rating,
+	});
+
+	req.flash("success_msg", "" + req.body.game_name + " has been updated successfully.");
+	res.redirect("/games/details/" + id);;
+});
+
 
 module.exports = router;
