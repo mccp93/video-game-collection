d576c4a29f514a7a42ae880b4576b610420a3ab4
Paul McCrory <paulmccrory@hotmail.co.uk>
2016-12-05 19:45:09 +0000

    Added user registration as well as a seperate db table on Firebase for user-info

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,58 @@
 var express = require('express');
 var router = express.Router();
+const Firebase = require('firebase');
 
-// Home Page
+// GET - Register
 router.get('/register', function(req, res, next) {
   	res.render('users/register');
 });
 
+// POST - Register
+router.post('/register', function(req, res, next) {
+  	var username = req.body.username;
+	var email = req.body.email;
+	var password = req.body.password;
+	var confirm_password = req.body.confirm_password;
+	var favourite_game = req.body.favourite_game;
+
+	// Validation
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email must be valid.').isEmail();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('confirm_password', 'Passwords must match.').equals(req.body.password);
+	
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('users/register', {errors: errors});
+	}else{
+		Firebase.auth().createUserWithEmailAndPassword(email, password).then(
+			function(data){
+				var user = {
+					uid: data.uid,
+					email: email,
+					username: username,
+					favourite_game: favourite_game
+				}
+
+				var userRef = Firebase.database().ref('users');
+				userRef.push().set(user);				
+
+				req.flash("success_msg", "You are now registered and may login.");
+				res.redirect('/users/login');
+			},
+			function(error) {
+				console.log("ERROR WHILE REGISTERING USER: "  + error);
+			}
+		);
+	}
+});
+
+// GET - Login
 router.get('/login', function(req, res, next) {
   	res.render('users/login');
 });
+
 
 module.exports = router;
